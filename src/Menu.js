cff735edc2b7df25f6d44783194fd57afd9c00c5
Maels1991 <r.wan.guillou+github@gmail.com>
2018-11-29 22:40:31 +0100

    Base files + Main Menu

---
+++ src/Menu.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const Menu = () => (
+  <ul>
+    <li>
+      <Link to="/" exact={true}>Home</Link>
+    </li>
+    <li>
+      <Link to="/about">
+        About
+        <ul>
+          <li>
+            <Link to="/about/me">About me</Link>
+          </li>
+          <li>
+            <Link to="/about/contact">Contact</Link>
+          </li>
+        </ul>
+      </Link>
+    </li>
+  </ul>
+);
+
+export default Menu;
