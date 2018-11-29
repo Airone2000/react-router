5b610aa2d88ead115d0d29e175830962735d9471
Maels1991 <r.wan.guillou+github@gmail.com>
2018-11-29 22:50:32 +0100

    Page about

---
+++ src/AboutContact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutContact = () => (
+  <h1>About contact ....</h1>
+);
+
+export default AboutContact;
--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import React from 'react';
 import Menu from './Menu';
 import {BrowserRouter as Router, Route} from 'react-router-dom';
 import Home from './Home';
+import About from './About';
+import AboutMe from './AboutMe';
+import AboutContact from './AboutContact';
 
 const App = () => (
   <Router>
@@ -11,7 +14,10 @@ const App = () => (
         <hr />
       </div>
 
-      <Route path="/" component={Home} />
+      <Route path="/" exact={true} component={Home} />
+      <Route path="/about" component={About} />
+      <Route path="/about/me" component={AboutMe} />
+      <Route path="/about/contact" component={AboutContact} />
     </React.Fragment>
   </Router>
 );
